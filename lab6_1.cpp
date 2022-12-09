6c7767f3931fe3093703577e4ac5bcca5b97026d
Sky <guymechatronic@gmail.com>
2022-12-09 15:34:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num =1;
+    int Even = 0;
+    int Odd = 0;
+    while(num != 0){
+     cout << "Enter an integer: ";
+     cin >> num;
+     if (num != 0){
+     if(num %2 == 0 && num != 0){
+        Even = Even + 1;
+     }
+     else if(num % 2!= 0 && num != 0) {
+        Odd = Odd+ 1;
+     }
+    }
+    }
+
+    cout << "#Even numbers = " << Even << "\n";
+    cout << "#Odd numbers = " << Odd << "\n";
     return 0;
 }
